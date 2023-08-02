ced1f361680aed29c6c975a010f5c770c49cb4ea
Fabricio Arce <fabarcsa@gmail.com>
2023-08-02 16:24:39 -0600

    Nuevo proyecto

---
+++ src/main/java/com/mycompany/proyecto_clinica_grupo6/Proyecto_Clinica_Grupo6.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.proyecto_clinica_grupo6;
+
+/**
+ *
+ * @author fabar
+ */
+public class Proyecto_Clinica_Grupo6 {
+
+    public static void main(String[] args) {
+        System.out.println("Hola Equipo grupo 6");
+    }
+}
