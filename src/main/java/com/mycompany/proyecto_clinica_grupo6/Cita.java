6524928e8e0baee034607327f3577d537d4c50e8
Sebastian Barboza <sbarboza30714@ufide.ac.cr>
2023-08-06 20:13:21 -0600

    Declaracion de variables y creacion del constructor

--- src/main/java/com/mycompany/proyecto_clinica_grupo6/Cita.java
+++ src/main/java/com/mycompany/proyecto_clinica_grupo6/Cita.java
@@ -10,4 +10,18 @@ package com.mycompany.proyecto_clinica_grupo6;
  */
 class Cita {
     
-}
+     // Estos datos se caracterizan por realizar un desglose a una cita: cliente, teléfono, servicio, duración, costo y el médico que atiende.
+    private String cliente;
+    private String telefono;
+    private String servicio;
+    private int duracion;
+    private double costo;
+    private Medico medico;
+    
+     // Constructor: es una función especial que nos permite crear o inicializar un objeto de tipo Cita con datos específicos.
+    public Cita(String cliente, String telefono, String servicio, Medico medico) {
+        this.cliente = cliente;
+        this.telefono = telefono;
+        this.servicio = servicio;
+        this.medico = medico;
+    }
